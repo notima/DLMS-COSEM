4e4d7e8788e71c3bb779d7b2ba479cb5d7014b42
Oliver Norin <oliver@notima.se>
2022-08-15 14:41:09 +0200

    added missing return statements

--- src/cosem.c
+++ src/cosem.c
@@ -1,10 +1,13 @@
 #include "cosem.h"
 #include <math.h>
 
+#include <stdio.h>
+
 struct cosem_data cosem_parse_data(struct dlms_object obj) {
     struct cosem_data result;
     result.logical_name = obj.payload.STRUCT[0].payload.OCTET_STRING;
     result.value = obj.payload.STRUCT[1].payload;
+    return result;
 }
 
 struct cosem_register cosem_parse_register(struct dlms_object obj) {
@@ -12,12 +15,14 @@ struct cosem_register cosem_parse_register(struct dlms_object obj) {
     result.logical_name = obj.payload.STRUCT[0].payload.OCTET_STRING;
     result.value = obj.payload.STRUCT[1].payload;
     result.scaler_unit = cosem_parse_scaler_unit(obj.payload.STRUCT[2]);
+    return result;
 }
 
 struct cosem_scaler_unit cosem_parse_scaler_unit(struct dlms_object obj) {
     struct cosem_scaler_unit result;
     result.scaler = *obj.payload.STRUCT[0].payload.INTEGER;
     result.unit = *obj.payload.STRUCT[1].payload.UNSIGNED;
+    return result;
 }
 
 double cosem_get_scaled_value(struct cosem_register reg) {
